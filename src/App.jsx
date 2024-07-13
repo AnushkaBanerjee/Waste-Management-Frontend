3a1f8c87875297c64442793d4bfb0e255c9fd94e
Anushka Banerjee <anushkabanerjee03@gmail.com>
2024-07-13 15:30:22 +0530

    chore: Update styles and components

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 export default function App() {
   return (
-    <h1 className="text-3xl  font-mono underline">
+    <h1 className="text-3xl font-bold font-mono underline">
       Hello world!
     </h1>
   )
