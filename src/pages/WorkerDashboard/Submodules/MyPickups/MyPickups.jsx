edcbf7f7621604b278e355b31fd78659c675c027
Ankit <ankit_b.cse2021@msit.edu.in>
2024-07-14 12:50:42 +0530

    feat: Update MyPickups component with styled table and data
    The MyPickups component has been updated to include a styled table and data. This change improves the visual presentation of the component by using the Material-UI Table component and populating it with sample data. The styled table enhances the user experience and provides a more organized and professional look to the MyPickups feature.

--- src/pages/WorkerDashboard/Submodules/MyPickups/MyPickups.jsx
+++ src/pages/WorkerDashboard/Submodules/MyPickups/MyPickups.jsx
@@ -4,33 +4,150 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Backend_url } from '../../../../../BackendUrl';
 import { useLoaderData } from 'react-router-dom';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import Chip from '@mui/material/Chip';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: "#059212",
+    color: theme.palette.common.white,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  // hide last border
+  '&:last-child td, &:last-child th': {
+    border: 0,
+  },
+}));
+
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
 
 function MyPickups
-() {
+  () {
   const [userData, setUserData] = useState();
-    const data = useLoaderData()
+  const data = useLoaderData()
+
   
-    useEffect(() => {
-      setUserData(data);
-    }, []);
+  const getCookie = (name) => {
+    const cookieString = document.cookie;
+    const cookies = cookieString.split('; ');
+    for (let cookie of cookies) {
+      const [cookieName, cookieValue] = cookie.split('=');
+      if (cookieName === name) {
+        return cookieValue;
+      }
+    }
+    return null;
+  };
+
+  const getPickups = async () => {
+    try {
+      const accessToken = getCookie('accessToken');
+      if (!accessToken) {
+        console.error("Access token not found");
+        return null;
+      }
+      const response = await axios.get(`${Backend_url}/api/v1/pickup/worker/view`, {
+        headers: {
+          Authorization: `Bearer ${accessToken}`
+        }
+      });
+      // console.log(response.data.data);
+
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+
+  useEffect(() => {
+    setUserData(data);
+    getPickups();
+  }, []);
   return (
-    <div style={{height: "calc(100vh - 4.3rem)",
+    <div style={{
+      height: "calc(100vh - 4.3rem)",
       width: "100%",
       display: "flex",
-      flexDirection:"column",
+      flexDirection: "column",
       padding: "1rem",
-      overflowY: "scroll",  
+      overflowY: "scroll",
       overflowX: "hidden",
-      background:"#EDE9E9"}}>
+      background: "#EDE9E9"
+    }}>
       <Banner customer={userData?.data.fullName} page="Explore New Pickups" />
-    
-        
-        
 
 
-       
-        
-      
+
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 700 }} aria-label="customized table" stickyHeader >
+          <TableHead>
+            <TableRow>
+              <StyledTableCell align="center"></StyledTableCell>
+              <StyledTableCell align="center">Name</StyledTableCell>
+              <StyledTableCell align="center">Pickup ID</StyledTableCell>
+              <StyledTableCell align="center">Status</StyledTableCell>
+              <StyledTableCell align="center">Action</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <StyledTableRow key={row.name}>
+                <StyledTableCell align="center">
+                  <div className='flex justify-center items-center'>
+                    <Avatar alt="Cindy Baker" src="https://i.pravatar.cc/150?u=a042581f4e29026024d" />
+                  </div>
+
+                </StyledTableCell>
+                <StyledTableCell align="center">{row.name}</StyledTableCell>
+                <StyledTableCell align="center">{row.calories}</StyledTableCell>
+                <StyledTableCell align="center">
+                <Chip label="accepted" color="warning" />
+                <Chip label="scheduled" color="primary" />
+                <Chip label="completed" color="success" />
+                </StyledTableCell>
+                <StyledTableCell align="center">
+                  <div className='flex justify-end gap-4'>
+                    <Button  variant="contained">Schedule Time</Button>
+                    <Button color="success" variant="contained">View</Button>
+                  </div>
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+
+
+
+
     </div>
   )
 }
